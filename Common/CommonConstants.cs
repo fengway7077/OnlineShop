384ca66051b6158edb1abbfa5cf103d0bf9cdd51
fengway7077 <fengway7077@yahoo.com>
2017-05-11 22:02:56 +0700

    2017/11/05
    2017/11/05

---
+++ Common/CommonConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+   public class CommonConstants
+    {
+        public static string MEMBER_GROUP= "MENBER";//ten giong database
+        public static string ADMIN_GROUP = "ADMIN";
+        public static string MOD_GROUP = "MOD";
+    }
+}
