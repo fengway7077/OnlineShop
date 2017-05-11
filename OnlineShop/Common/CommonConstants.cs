59b689d60e6ffe35b814a5ef7c86d4a325c30667
fengway7077 <fengway7077@yahoo.com>
2017-05-11 23:18:52 +0700

    2017/05/11
    2017/05/11

---
+++ OnlineShop/Common/CommonConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Common
+{
+    public static class CommonConstants
+    {
+        //static
+        public static string USER_SESSION = "USER_SESSION";
+        //phan quyen
+        public static string SESSION_CREDENTIALS = "SESSION_CREDENTIALS";
+        public static string CartSession = "CartSession";//headercart
+
+        //xu ly language
+        public static string CurrentCulture { set; get; }
+    }
+}
