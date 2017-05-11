0a7de017e9344af43b567a672b78578ec00d2e80
fengway7077 <fengway7077@yahoo.com>
2017-05-11 23:14:03 +0700

    2017/05/11
    2017/05/11

---
+++ OnlineShop/Models/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//
+using Model.EF;
+
+namespace OnlineShop.Models
+{
+    [Serializable]// de tu tu hoa, session
+    public class CartItem
+    {
+       //// public long ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ OnlineShop/Models/RegisterModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Models
+{
+    public class RegisterModel
+    {
+        [Key]
+        public long ID { set; get; }
+
+        [Display(Name = " Tên đăng nhập")]
+        [Required(ErrorMessage = "Bạn phải nhập tên đăng nhập")]
+        public string UserName { get; set; }
+
+        [Display(Name = " Mật khẩu")]
+        [StringLength(20, MinimumLength = 6,ErrorMessage = "Độ dài mật khẩu từ 6 đến 20 ký tự")]
+        [Required(ErrorMessage = "Bạn phải nhập mật khẩu")]
+        public string Password { get; set; }
+
+        [Display(Name = " Xác nhận mật khẩu")]
+        [Compare("Password",ErrorMessage = "Xác nhận mật khẩu không đúng")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = " Họ tên")]
+        [Required(ErrorMessage = "Bạn phải nhập tên")]
+        public string Name { get; set; }
+
+        [Display(Name = " Địa chỉ")]
+        public string Address { get; set; }
+
+        [Display(Name = " Email")]
+        [Required(ErrorMessage = "Bạn phải nhập Email")]
+        public string Email { get; set; }
+        [Display(Name = " Điện thoại")]
+        public string Phone { get; set; }
+        [Display(Name = " Tỉnh/Thành")]
+        public string ProvinceID { get; set; }
+
+        [Display(Name = "Quận/Huyện")]
+        public string DistrictID { get; set; }
+
+    }
+}
