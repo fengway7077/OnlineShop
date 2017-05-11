9874c11e21e26577e61fdf4be0575a85e3db3de6
fengway7077 <fengway7077@yahoo.com>
2017-05-11 23:21:00 +0700

    2017/05/11
    2017/05/11

---
+++ OnlineShop/App_Start/BundleConfig.cs
@@ -0,0 +1,51 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace OnlineShop
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //js 
+            bundles.Add(new ScriptBundle("~/bundles/jscore").Include(
+                     
+                        "~/assets/client/js/jquery-1.11.3.min.js",
+                       
+                        "~/assets/client/js/jquery-ui.js",
+                        "~/assets/client/js/bootstrap.min.js",
+                     //   "~/assets/client/js/jquery-1.7.2.min.js",
+                        "~/assets/client/js/move-top.js",
+                        "~/assets/client/js/easing.js",
+                        "~/assets/client/js/startstop-slider.js",
+                     //   "~/assets/client/nivo-slider/demo/scripts/jquery-1.9.0.min.js",
+                        "~/assets/client/nivo-slider/jquery.nivo.slider.js"
+                        ));
+            bundles.Add(new ScriptBundle("~/bundles/controller").Include(
+                "~/assets/client/js/controller/baseController.js"
+               
+                ));
+            //css o day
+        
+          bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/assets/client/css/bootstrap.css",
+                      "~/assets/client/css/social-buttons.css",
+                      "~/assets/client/css/font-awesome.min.css",
+                      "~/assets/client/css/bootstrap-theme.css",
+                      "~/assets/client/css/jquery-ui.css",
+                      "~/assets/client/css/style.css",
+                       "~/assets/client/css/slider.css",//
+                  "~/assets/client/nivo-slider/themes/default/default.css",
+                 "~/assets/client/nivo-slider/themes/light/light.css",
+                "~/assets/client/nivo-slider/themes/dark/dark.css",
+                "~/assets/client/nivo-slider/themes/bar/bar.css",
+                "~/assets/client/nivo-slider/nivo-slider.css",
+                "~/assets/client/nivo-slider/demo/style1.css"
+
+                      ));
+         
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ OnlineShop/App_Start/RouteConfig.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OnlineShop
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            routes.MapRoute(
+                name: "Products All",
+                url: "san-pham",//
+                defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "OnlineShop.Controllers" }
+            );
+
+
+            routes.MapRoute(
+                name: "Product Category",
+                url: "san-pham/{metatitle}-{cateid}",//ProductController dung cateid
+                defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
+                namespaces: new[] { "OnlineShop.Controllers" }
+            );
+            routes.MapRoute(
+              name: "Product Detail",
+              url: "chi-tiet/{metatitle}-{dtid}",//ProductController dung dtid
+              defaults: new { controller = "Product", action = "Detail", id = UrlParameter.Optional },
+              namespaces: new[] { "OnlineShop.Controllers" }
+          );
+            routes.MapRoute(
+              name: "About",
+              url: "them-gio-hang",//trang khong co tham so gioi thieu
+              defaults: new { controller = "Cart", action = "AddItem", id = UrlParameter.Optional },
+              namespaces: new[] { "OnlineShop.Controllers" }
+          );
+            routes.MapRoute(
+              name: "Content",
+              url: "tin-tuc",//trang khong co tham so tin tuc
+              defaults: new { controller = "Content", action = "Index", id = UrlParameter.Optional },
+              namespaces: new[] { "OnlineShop.Controllers" }
+          );
+            routes.MapRoute(
+             name: "Contact",
+             url: "lien-he",//trang khong co tham so lien he
+             defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional },
+             namespaces: new[] { "OnlineShop.Controllers" }
+         );
+            routes.MapRoute(
+            name: "Login",
+            url: "dang-nhap",//trang khong co tham so 
+            defaults: new { controller = "User", action = "Login", id = UrlParameter.Optional },
+            namespaces: new[] { "OnlineShop.Controllers" }
+        );
+            routes.MapRoute(
+           name: "Sign In",
+           url: "dang-ky",//trang khong co tham so 
+           defaults: new { controller = "User", action = "Register", id = UrlParameter.Optional },
+           namespaces: new[] { "OnlineShop.Controllers" }
+       );
+            routes.MapRoute(
+           name: "Search",
+           url: "tim-kiem",//trang khong co tham so
+           defaults: new { controller = "Product", action = "Search", id = UrlParameter.Optional },
+           namespaces: new[] { "OnlineShop.Controllers" }
+       );
+            routes.MapRoute(
+             name: "Cart",
+             url: "gio-hang",//trang khong co tham so
+             defaults: new { controller = "Cart", action = "Index", id = UrlParameter.Optional },
+             namespaces: new[] { "OnlineShop.Controllers" }
+         );
+            routes.MapRoute(
+            name: "Payment",
+            url: "thanh-toan",//trang khong co tham so
+            defaults: new { controller = "Cart", action = "Payment", id = UrlParameter.Optional },
+            namespaces: new[] { "OnlineShop.Controllers" }
+        );
+            routes.MapRoute(
+              name: "Add Cart",
+              url: "gioi-thieu",//trang khong co tham so
+              defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
+              namespaces: new[] { "OnlineShop.Controllers" }
+          );
+            routes.MapRoute(
+            name: "Payment Success",
+            url: "hoan-thanh",//trang khong co tham so
+            defaults: new { controller = "Cart", action = "Success", id = UrlParameter.Optional },
+            namespaces: new[] { "OnlineShop.Controllers" }
+        );
+            //Default luon de duoi cung
+            routes.MapRoute(
+             name: "Default",
+             url: "{controller}/{action}/{id}",
+             defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+             namespaces: new[] { "OnlineShop.Controllers" }//chi ra controller chay mat dinh
+         );
+        }
+    }
+}
